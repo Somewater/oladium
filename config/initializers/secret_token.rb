e6c9837ec241a4cc4f40a250c199044048355544
Somewater <mktsz@mail.ru>
2012-07-07 02:11:40 +0400

    Инициализация rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oladium::Application.config.secret_token = 'd535ca65a10b3e78957451a04befb909aad3358f528bf2bb36134d81413348cf6232d4e8267a41b4915e3cdd9f0121eff7bca7388d42c6d3efda84d13c789ad8'
