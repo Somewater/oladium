c98e091b3904cd94641d9ab4c72362c953db42a5
Pavel Naydenov <naydenov.p.v@gmail.com>
2013-07-13 13:29:39 +0400

    switch off registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :recoverable
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :rememberable, :validatable
 
   # Setup accessible (or protected) attributes for your model
