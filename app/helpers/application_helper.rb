7fbcbf99fe841cee317bce6f95aced588bb7492e
Pavel Naydenov <naydenov.p.v@gmail.com>
2014-03-26 01:29:38 +0400

    paginator fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,26 @@ module ApplicationHelper
     url_for(self.params.merge(params))
   end
 
+  def each_with_borders(array, &block)
+    last_index = array.size - 1
+    index = 0
+    array.each do |item|
+      block.call(item, index, index == last_index)
+      index += 1
+    end
+  end
+
+  def paginate_collection(collection, current, page_half_quantity, &block)
+    length = collection.length
+    index = collection.index(current)
+    start_index = [index - page_half_quantity, 0].max
+    end_index = [index + page_half_quantity, length].min
+    each_with_borders(collection[start_index...end_index]) do |item, idx, last|
+      item_index = start_index + idx
+      block.call(item, item_index, idx == 0 && item_index > 0, last && item_index < length - 1)
+    end
+  end
+
   private
   def game_to_hash(game)
     h = { :net => game.net, :net_id => game.net_id, :title => game.title, :description => game.description,
