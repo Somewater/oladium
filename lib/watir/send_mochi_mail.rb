eced1392b486ab8a18398d3eafa6da84cd65fff9
Pavel Naydenov <naydenov.p.v@gmail.com>
2014-03-26 00:35:42 +0400

    watir fixes

--- lib/watir/send_mochi_mail.rb
+++ lib/watir/send_mochi_mail.rb
@@ -11,15 +11,37 @@ class SendMochiMail
     @browser.text_field(:id, 'email').set('mktsz@mail.ru')
     @browser.text_field(:id, 'password').set('adelaida')
     @browser.button(:text, 'Login').click
+
+    require 'logger'
+    @logger = Logger.new("#{Rails.root}/tmp/watir_send_mochi_mail.log")
   end
 
   def email(user_link, title, body)
     #@browser.goto "https://www.mochimedia.com/community/profile/Evil-Dog"
+    @logger.info "Email start to '#{user_link}'"
     @browser.goto user_link
+
+    stop_alerts!
+
     @browser.link(:text, "Private Message").click
     @browser.text_field(:id, "msg_subject").set(title)
     @browser.text_field(:id, "msg_body").set(body)
     @browser.button(:id, "b_send").click
+    @logger.info "Emailed link '#{user_link}'"
+    #@browser.speed = :zippy
+  end
+
+  def stop_alerts!
+    # don't return anything for alert
+    @browser.execute_script("window.alert = function() {}")
+    # return some string for prompt to simulate user entering it
+    @browser.execute_script("window.prompt = function() {return 'my name'}")
+    # return null for prompt to simulate clicking Cancel
+    @browser.execute_script("window.prompt = function() {return null}")
+    # return true for confirm to simulate clicking OK
+    @browser.execute_script("window.confirm = function() {return true}")
+    # return false for confirm to simulate clicking Cancel
+    @browser.execute_script("window.confirm = function() {return false}")
   end
 
   def process_games(games)
@@ -44,7 +66,8 @@ class SendMochiMail
     end
   end
 
-  def process_file(file_name)
+  def process_file(file_name = 'authors.txt')
+    @browser.speed = :zippy
     File.open(file_name) do |f|
       f.each_line do |line|
         arr = line.split(';;;;')
@@ -52,17 +75,23 @@ class SendMochiMail
         author_link = arr[1]
         sig = arr[2]
         games = arr.drop(3)
-        email(author_link, "Your game#{games.size > 1 ? 's' : ''} publishing", get_message(author, games, sig))
+        email(author_link, "Publishing your game#{games.size > 1 ? 's' : ''}", get_message(author, games, sig))
       end
     end
   end
 
   def get_message(author, games, sig)
     s = games.size > 1 ? 's' : ''
+    games = games.each_with_index.to_a.select{|g,idx| idx % 2 == 0}.map{|g,idx| g.strip}
+    if games.size <= 3
+      games_str = games.map{|g| '"' + g + '"'}.join(', ')
+    else
+      games_str = games.take(2).map{|g| '"' + g + '"'}.join(', ').to_s.dup + ", etc"
+    end
     m = <<-MSG
 Hello!
 As you probably know Mochigames finalizing distribution of games in the coming days (http://mochiland.com/articles/mochi-media-winding-down-services-end-date-of-3-31-2014).
-Your game#{s} #{games.map{|g| '"' + g + '"'}.join(', ')} is published on our web site oladium.com and we offer further cooperation in distribution of the game#{s}.
+Your game#{s} #{games_str} is published on our web site http://oladium.com and we offer further cooperation in distribution of the game#{s}.
 Please contact us or upload version of the game without integrated Mochi Ad. We accept games for publication with any built in ads.
 Email for communication: games@oladium.com
 Link to enter the profile on the site for game uploads (under development, will be available in the next few days) :
