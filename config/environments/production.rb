2310d6e29abc4a6e3282464e8619f2373e45b3c7
Pavel Naydenov <pav@atlantor.ru>
2014-03-30 20:52:59 +0000

    fix password cnahge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ Oladium::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => "oladium.com" }
 
   # Enable threaded mode
   # config.threadsafe!
