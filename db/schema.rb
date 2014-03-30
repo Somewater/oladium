9cdeba425a515ffd55c0279bab7d20176a13b75a
Pavel Naydenov <pav@atlantor.ru>
2014-03-30 16:28:50 +0000

    uniq slug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140323232906) do
+ActiveRecord::Schema.define(:version => 20140330141921) do
 
   create_table "categories", :force => true do |t|
     t.string "name",     :null => false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20140323232906) do
   end
 
   add_index "games", ["net", "net_id"], :name => "index_games_on_net_and_net_id", :unique => true
+  add_index "games", ["slug"], :name => "index_games_on_slug", :unique => true
 
   create_table "minecraft_users", :force => true do |t|
     t.string   "login",                            :null => false
